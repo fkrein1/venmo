fce1169a523adc12f4336a33ba78008709da4efc
fkrein1 <felipekrein@gmail.com>
2022-11-21 08:27:59 -0300

    refactor: remove useAuth hook

--- frontend/src/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-import { AuthoContext } from '../context/authContext'
-
-export function useAuth () {
-  const value = useContext(AuthoContext)
-
-  return value
-}
