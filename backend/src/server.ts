bde79af6d2587f842e8bbdd0eedc6c5a3fa0574d
fkrein1 <felipekrein@gmail.com>
2022-11-21 13:13:34 -0300

    feat: swagger documentation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import cors from "cors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 import { errorMiddleware } from "./errors/errorMiddleware";
 import routes from "./routes";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerFile = require("./swagger.json");
 
 const PORT = process.env.PORT || 3008;
 
@@ -11,6 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.use(errorMiddleware);
