bf04a25497208fb67bc6746f1a7563053ab7e90c
fkrein1 <felipekrein@gmail.com>
2022-11-22 04:45:49 -0300

    fix: superagent types

---
+++ backend/src/@types/superagent/index.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+// error TS2304: Cannot find name 'XMLHttpRequest'
+declare interface XMLHttpRequest {}
+// error TS2304: Cannot find name 'Blob'
+declare interface Blob {}
